6e29bfbd6d7fedacc5cb344ea74876bcb0fd4de8
hrmk1o3 <hiruma@toycash.awsapps.com>
2021-12-27 14:42:58 +0900

    add debug environment

--- src/main.rs
+++ src/main.rs
@@ -9,3 +9,14 @@ fn main() {
   run_with_file_path::<_, _, _, BlakeDigest>(r1cs_file_path, witness_file_path, proof_json_path)
     .unwrap();
 }
+
+#[test]
+fn test_main_func_to_run_compute() {
+  use commitment::blake::BlakeDigest;
+
+  let r1cs_file_path = "./packages/r1cs-stark/tests/compute.r1cs";
+  let witness_file_path = "./packages/r1cs-stark//tests/compute.wtns";
+  let proof_json_path = "./packages/r1cs-stark//tests/compute_proof.json";
+  run_with_file_path::<_, _, _, BlakeDigest>(r1cs_file_path, witness_file_path, proof_json_path)
+    .unwrap();
+}
